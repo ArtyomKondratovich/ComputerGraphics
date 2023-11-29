997bdb13146d62e8fd685c062a5b0cb73709ae84
ArtyomKondratovich <kondratovich443@gmail.com>
2023-11-29 16:56:48 +0300

    added lab 3

---
+++ lab3/code/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿using OxyPlot.Axes;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstPoint = new System.Windows.Forms.TextBox();
+            this.secondPoint = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.scale = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bresenham = new System.Windows.Forms.CheckBox();
+            this.stepBystep = new System.Windows.Forms.CheckBox();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            ((System.ComponentModel.ISupportInitialize)(this.scale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(905, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Point 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // firstPoint
+            // 
+            this.firstPoint.Location = new System.Drawing.Point(908, 37);
+            this.firstPoint.Name = "firstPoint";
+            this.firstPoint.Size = new System.Drawing.Size(100, 22);
+            this.firstPoint.TabIndex = 1;
+            // 
+            // secondPoint
+            // 
+            this.secondPoint.Location = new System.Drawing.Point(908, 81);
+            this.secondPoint.Name = "secondPoint";
+            this.secondPoint.Size = new System.Drawing.Size(100, 22);
+            this.secondPoint.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(905, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Point 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(908, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Paint";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // scale
+            // 
+            this.scale.Location = new System.Drawing.Point(908, 226);
+            this.scale.Maximum = 200;
+            this.scale.Name = "scale";
+            this.scale.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.scale.Size = new System.Drawing.Size(56, 104);
+            this.scale.TabIndex = 7;
+            this.scale.Value = 100;
+            this.scale.Scroll += new System.EventHandler(this.Scale_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(908, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Time: 0";
+            // 
+            // bresenham
+            // 
+            this.bresenham.AutoSize = true;
+            this.bresenham.Checked = true;
+            this.bresenham.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bresenham.Location = new System.Drawing.Point(908, 109);
+            this.bresenham.Name = "bresenham";
+            this.bresenham.Size = new System.Drawing.Size(156, 20);
+            this.bresenham.TabIndex = 9;
+            this.bresenham.Text = "Bresenham algorithm";
+            this.bresenham.UseVisualStyleBackColor = true;
+            this.bresenham.CheckedChanged += new System.EventHandler(this.Bresenham_CheckedChanged);
+            // 
+            // stepBystep
+            // 
+            this.stepBystep.AutoSize = true;
+            this.stepBystep.Location = new System.Drawing.Point(908, 135);
+            this.stepBystep.Name = "stepBystep";
+            this.stepBystep.Size = new System.Drawing.Size(164, 20);
+            this.stepBystep.TabIndex = 10;
+            this.stepBystep.Text = "Step-by-step algorithm";
+            this.stepBystep.UseVisualStyleBackColor = true;
+            this.stepBystep.CheckedChanged += new System.EventHandler(this.StepBystep_CheckedChanged);
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(12, 18);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(870, 537);
+            this.plotView1.TabIndex = 11;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 592);
+            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.stepBystep);
+            this.Controls.Add(this.bresenham);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.scale);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.secondPoint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.firstPoint);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.scale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstPoint;
+        private System.Windows.Forms.TextBox secondPoint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar scale;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox bresenham;
+        private System.Windows.Forms.CheckBox stepBystep;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+    }
+}
+
---
+++ lab3/code/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OxyPlot.Axes;
+using OxyPlot.Annotations;
+using OxyPlot;
+using OxyPlot.Utilities;
+using System.Runtime.CompilerServices;
+using OxyPlot.Series;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace lab_3 {
+
+    public partial class Form1 : Form
+    {
+        private Point point1;
+        private Point point2;
+
+        public bool IsEvent = true;
+
+        public PlotModel PlotModel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Создание осей координат
+            var xAxis = new LinearAxis
+            {
+                Position = AxisPosition.Bottom, // Ось X внизу
+                Title = "X", // Заголовок оси X
+                Minimum = -1,
+                Maximum = 10,
+            };
+            var yAxis = new LinearAxis
+            {
+                Position = AxisPosition.Left, // Ось Y слева
+                Title = "Y", // Заголовок оси Y
+                Minimum = -1,
+                Maximum = 10,
+            };
+            PlotModel = new PlotModel();
+            PlotModel.Axes.Add(xAxis);
+            PlotModel.Axes.Add(yAxis);
+            xAxis.MajorGridlineStyle = LineStyle.Solid;
+            xAxis.MinorGridlineStyle = LineStyle.Dot;
+            yAxis.MajorGridlineStyle = LineStyle.Solid;
+            yAxis.MinorGridlineStyle = LineStyle.Dot;
+            xAxis.MajorGridlineColor = OxyColor.FromRgb(200, 200, 200); // Цвет основных линий сетки по оси X
+            xAxis.MinorGridlineColor = OxyColor.FromRgb(230, 230, 230); // Цвет вспомогательных линий сетки по оси X
+            yAxis.MajorGridlineColor = OxyColor.FromRgb(200, 200, 200); // Цвет основных линий сетки по оси Y
+            yAxis.MinorGridlineColor = OxyColor.FromRgb(230, 230, 230); // Цвет вспомогательных линий сетки по оси Y
+            PlotModel.PlotAreaBorderColor = OxyColor.FromRgb(150, 150, 150); // Цвет границы области графика
+            plotView1.Model = PlotModel;
+
+            PlotModel.PlotType = PlotType.Cartesian;
+            PlotModel.Background = OxyColors.White;
+            PlotModel.PlotAreaBorderColor = OxyColors.LightGray;
+
+            xAxis.MajorGridlineStyle = LineStyle.Solid;
+            xAxis.MinorGridlineStyle = LineStyle.Dot;
+            yAxis.MajorGridlineStyle = LineStyle.Solid;
+            yAxis.MinorGridlineStyle = LineStyle.Dot;
+
+            PlotModel.PlotAreaBorderColor = OxyColor.FromRgb(150, 150, 150);
+            
+            plotView1.Model = PlotModel;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scale_Scroll(object sender, EventArgs e)
+        {
+            label3.Text = $"Scale: {scale.Value}";
+        }
+
+        private void Bresenham_CheckedChanged(object sender, EventArgs e)
+        {
+            if (IsEvent)
+            {
+                IsEvent = false;
+                stepBystep.Checked = !bresenham.Checked;
+                IsEvent = true;
+            }
+        }
+
+        private void StepBystep_CheckedChanged(object sender, EventArgs e)
+        {
+            if (IsEvent)
+            {
+                IsEvent = false;
+                bresenham.Checked = !stepBystep.Checked;
+                IsEvent = true;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (TryParse(firstPoint.Text, out point1) && 
+                TryParse(secondPoint.Text, out point2))
+            {
+                DrawSquare(point1);
+                DrawSquare(point2);
+
+                var time = new Stopwatch();
+
+                time.Start();
+                var points = bresenham.Checked ? DrawLineBresenham(point1,point2) : DrawLinePointByPoint(point1, point2);
+                time.Stop();
+
+                foreach (var point in points)
+                {
+                    DrawSquare(point);
+                }
+
+                label3.Text = $"Time: {time.ElapsedMilliseconds} ms";
+            }
+        }
+
+        private static bool TryParse(string s, out Point point)
+        {
+            var coordinates = s.Split(',');
+
+            if (coordinates.Length == 2 &&
+                int.TryParse(coordinates[0], out var x) &&
+                int.TryParse(coordinates[1],out var y))
+            {
+                point = new Point(x, y);
+                return true;
+            }
+
+            point = new Point(-1, -1);
+            return false;
+        }
+
+        private void DrawSquare(Point point)
+        {
+            var rectangle = new RectangleBarSeries
+            {
+                StrokeThickness = 1, // Толщина обводки
+                StrokeColor = OxyColors.Black, // Цвет обводки
+                FillColor = OxyColors.Blue // Цвет заливки
+            };
+
+            rectangle.Items.Add(new RectangleBarItem
+            {
+                X0 = point.X, // Левая координата X
+                X1 = point.X + 1, // Правая координата X
+                Y0 = point.Y, // Верхняя координата Y
+                Y1 = point.Y - 1  // Нижняя координата Y
+            });
+
+            PlotModel.Series.Add(rectangle);
+            plotView1.InvalidatePlot(true);
+        }
+
+        public static List<Point> DrawLinePointByPoint(Point p1, Point p2)
+        {
+            int dx = p2.X - p1.X;
+            int dy = p2.Y - p1.Y;
+
+            int x = p1.X;
+            int y = p1.Y;
+
+            List<Point> points = new List<Point>();
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                bool steep = Math.Abs(dy) > Math.Abs(dx);
+                if (steep)
+                {
+                    Swap(ref x, ref y);
+                    Swap(ref p1);
+                    Swap(ref p2);
+                    dx = p2.X - p1.X;
+                    dy = p2.Y - p1.Y;
+                }
+                if (p1.X > p2.X)
+                {
+                    SwapX(ref p1, ref p2);
+                    SwapY(ref p1, ref p2);
+                    dx = p2.X - p1.X;
+                    dy = p2.Y - p1.Y;
+                }
+                int d = 2 * Math.Abs(dy) - Math.Abs(dx);
+                int yStep = p1.Y > p2.Y ? -1 : 1;
+                while (x <= p2.X)
+                {
+                    points.Add(steep ? new Point(y, x) : new Point(x, y));
+                    if (d > 0)
+                    {
+                        y += yStep;
+                        d -= 2 * Math.Abs(dx);
+                    }
+                    x += 1;
+                    d += 2 * Math.Abs(dy);
+                }
+            }
+            else
+            {
+                bool steep = Math.Abs(dx) > Math.Abs(dy);
+                if (steep)
+                {
+                    Swap(ref x, ref y);
+                    Swap(ref p1);
+                    Swap(ref p2);
+                    dx = p2.X - p1.X;
+                    dy = p2.Y - p1.Y;
+                }
+                if (p1.Y > p2.Y)
+                {
+                    SwapY(ref p1, ref p2);
+                    SwapX(ref p1, ref p2);
+                    dy = p2.Y - p1.Y;
+                    dx = p2.X - p1.X;
+                }
+                int d = 2 * Math.Abs(dx) - Math.Abs(dy);
+                int xStep = p1.X > p2.X ? -1 : 1;
+                while (y <= p2.Y)
+                {
+                    points.Add(steep ? new Point(y, x) : new Point(x, y));
+                    if (d > 0)
+                    {
+                        x += xStep;
+                        d -= 2 * Math.Abs(dy);
+                    }
+                    y += 1;
+                    d += 2 * Math.Abs(dx);
+                }
+            }
+
+            return points;
+        }
+
+        public static List<Point> DrawLineBresenham(Point p1, Point p2)
+        {
+            int dx = Math.Abs(p2.X - p1.X);
+            int dy = Math.Abs(p2.Y - p1.Y);
+
+            bool steep = dy > dx;
+
+            if (steep)
+            {
+                Swap(ref p1);
+                Swap(ref p2);
+                dx = Math.Abs(p2.X - p1.X);
+                dy = Math.Abs(p2.Y - p1.Y);
+            }
+
+            if (p1.X > p2.X)
+            {
+                SwapX(ref p1, ref p2);
+                SwapY(ref p1, ref p2);
+            }
+
+            int yStep = p1.Y < p2.Y ? 1 : -1;
+            int d = 2 * dy - dx;
+            int y = p1.Y;
+
+            List<Point> points = new List<Point>();
+            for (int x = p1.X; x <= p2.X; x++)
+            {
+                points.Add(steep ? new Point(y, x) : new Point(x, y));
+
+                if (d > 0)
+                {
+                    y += yStep;
+                    d -= 2 * dx;
+                }
+                d += 2 * dy;
+            }
+
+            return points;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Swap(ref Point point)
+        {
+            int temp = point.X;
+            point.X = point.Y;
+            point.Y = temp;
+        }
+
+        public static void SwapX(ref Point point1, ref Point point2)
+        {
+            int temp = point1.X;
+            point1.X = point2.X;
+            point2.X = temp;
+        }
+
+        public static void SwapY(ref Point point1, ref Point point2)
+        {
+            int temp = point1.Y;
+            point1.Y = point2.Y;
+            point2.Y = temp;
+        }
+
+    }
+}
